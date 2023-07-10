f657949ada5845248f43139784d065eeffda86eb
angelotax <angelotax@gmail.com>
2023-07-11 00:32:21 +0200

    refactor

--- steps/src/App.js
+++ steps/src/App.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     setStep(step >= 2 ? step - 1 : step);
@@ -16,30 +17,39 @@ export default function App() {
     setStep(step >= 3 ? step : step + 1);
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? 'active' : ''}>1</div>
-        <div className={step >= 2 ? 'active' : ''}>2</div>
-        <div className={step >= 3 ? 'active' : ''}>3</div>
-      </div>
+    // fragment allows more than one element to be returned by a react component, check this one below
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: '#7950f2', color: '#ffff' }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: '#7950f2', color: '#ffff' }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#ffff' }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#ffff' }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+      {/* fragment closure here */}
+    </>
   );
 }
