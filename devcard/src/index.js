d115d074c1b862caa9be2843c5686079d3b8d834
angelotax <angelotax@gmail.com>
2023-07-10 15:23:04 +0200

    devcardChallenge2

--- devcard/src/index.js
+++ devcard/src/index.js
@@ -1,6 +1,39 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
 function App() {
   return (
@@ -40,24 +73,27 @@ function Intro() {
 }
 function SkillList() {
   return (
-    <div className="skill-list">
-      <Skill skill="React" emoji="👌" color="violet" />
-      <Skill skill="HTML+CSS" emoji="💪" color="yellow" />
-      <Skill skill="Javascript" emoji="👍" color="red" />
-      <Skill skill="Web-Design" emoji="🤟" color="green" />
-    </div>
+    <ul className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </ul>
   );
 }
-function Skill(props) {
+function Skill({ skill, color, level }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "😊"}
+        {level === "intermediate" && "👌"}
+        {level === "advanced" && "👍"}
+      </span>
     </div>
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
