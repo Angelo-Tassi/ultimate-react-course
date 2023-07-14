8d79e577153d365f92d74cd96b7fe19437bb6e79
angelotax <angelotax@gmail.com>
2023-07-14 16:23:56 +0200

    counter upgraded

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
-import { useState } from 'react';
+import "./index.css";
+import "./App.css";
+import { useState } from "react";
 
 export default function App() {
   return (
@@ -18,20 +19,28 @@ function Multiplier() {
   }
   return (
     <div>
-      <div>
-        <button onClick={subtractFive}>-</button>
-        <span>Step:{step5}</span>
-        <button onClick={addFive}>+</button>
+      <div class="reset">
+        <input
+          class="reset"
+          type="range"
+          min="1"
+          max="10"
+          value={step5}
+          onChange={(e) => setStep5(Number(e.target.value))}
+        ></input>
+        {/* <button onClick={subtractFive}>-</button> */}
+        <span class="reset">Step:{step5}</span>
+        {/* <button onClick={addFive}>+</button> */}
       </div>
-      <Counter step5={step5} />
+      <Counter step5={step5} setStep5={setStep5} />
     </div>
   );
 }
 
-function Counter({ step5 }) {
+function Counter({ step5, setStep5 }) {
   const [step, setStep] = useState(0);
 
-  const date = new Date('June 21 2027');
+  const date = new Date("June 21 2027");
   date.setDate(date.getDate() + step);
 
   function addSingle() {
@@ -40,25 +49,46 @@ function Counter({ step5 }) {
   function subtractSingle() {
     setStep((s) => s - step5);
   }
+  function handleReset() {
+    setStep(0);
+    setStep5(1);
+  }
 
   return (
     <div>
       <div>
-        <button onClick={subtractSingle}>-</button>
-        <span>Count:{step}</span>
-        <button onClick={addSingle}>+</button>
+        <button class="reset" onClick={subtractSingle}>
+          -
+        </button>
+        <input
+          class="reset"
+          type="text"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+        <button class="reset" onClick={addSingle}>
+          +
+        </button>
       </div>
-
-      <p>
+      <p class="reset">
         <span>
           {step === 0
-            ? 'Today is '
+            ? "Today is "
             : step > 0
             ? `${step} days from today is `
             : `${Math.abs(step)} days ago was `}
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      {(step === 0) & (step5 === 1) ? (
+        ""
+      ) : (
+        <div>
+          <button class="reset" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ counter/src/updatedCounter.js
@@ -0,0 +1,64 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Multiplier />
+    </div>
+  );
+}
+function Multiplier() {
+  const [step5, setStep5] = useState(1);
+  function addFive() {
+    setStep5((s) => s + 1);
+  }
+  function subtractFive() {
+    if (step5 >= 2) setStep5((s) => s - 1);
+  }
+  return (
+    <div>
+      <div>
+        <button onClick={subtractFive}>-</button>
+        <span>Step:{step5}</span>
+        <button onClick={addFive}>+</button>
+      </div>
+      <Counter step5={step5} />
+    </div>
+  );
+}
+
+function Counter({ step5 }) {
+  const [step, setStep] = useState(0);
+
+  const date = new Date("June 21 2027");
+  date.setDate(date.getDate() + step);
+
+  function addSingle() {
+    setStep((s) => s + step5);
+  }
+  function subtractSingle() {
+    setStep((s) => s - step5);
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={subtractSingle}>-</button>
+        <span>Count:{step}</span>
+        <button onClick={addSingle}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {step === 0
+            ? "Today is "
+            : step > 0
+            ? `${step} days from today is `
+            : `${Math.abs(step)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
