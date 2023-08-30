ec7eb544932aea05625da7f3ede9551312cdad1d
Angelo Tassi <angelotax@gmail.com>
2023-08-30 12:12:14 +0200

    second comm

--- 06-eat-n-split/starter/src/App.js
+++ 06-eat-n-split/starter/src/App.js
@@ -46,8 +46,16 @@ function Friend({ friend }) {
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {friend.balance}Euro
+          You owe {friend.name} {Math.abs(friend.balance)} $
         </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)} $
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p className="green">You and {friend.name} are even</p>
       )}
     </li>
   );
